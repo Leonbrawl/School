3849b6631725daaa47946983ae81e669a6cfb2f4
wh0o0o <wh0o.0o1320@gmail.com>
2023-05-04 11:14:18 +0300

    Добавьте файлы проекта.

---
+++ School/Pages/SchoolData.xaml.cs
@@ -0,0 +1,47 @@
+﻿using School.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace School.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для SchoolData.xaml
+    /// </summary>
+    public partial class SchoolData : Page
+    {
+        public SchoolData()
+        {
+            InitializeComponent();
+            DataGrid.ItemsSource = App.db.Dance.ToList();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedProduct = DataGrid.SelectedItem as Dance;
+            if (selectedProduct != null)
+            {
+                MessageBox.Show("Выберите товар");
+                return;
+            }
+            selectedProduct.IsDelete = true;
+            App.db.SaveChanges();
+        }
+    }
+}
